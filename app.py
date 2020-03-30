3f2bb11092f58c87c87a74e59b9341315a9831d5
brosnans <s.brosnan3@gmail.com>
2020-03-30 17:37:39 +0000

    Connected to DB

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'incident_manager'
+app.config["MONGO_URI"] = 'mongodb+srv://user1:welcomeuser1@firstcluster-kacbb.mongodb.net/incident_manager?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
 
 @app.route('/')
-def hello():
-    return 'Hello World ...again'
+@app.route('/get_incidents')
+def get_incidents():
+    return render_template("incidents.html", incidents=mongo.db.incidents.find())
 
 
 if __name__ == '__main__':
