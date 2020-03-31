2fb36975376d38fb00136e8d6ace668046dc0da2
brosnans <s.brosnan3@gmail.com>
2020-03-31 22:41:24 +0000

    Added env.py

---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+
+app.config["MONGO_DBNAME"] = 'incident_manager'
+app.config["MONGO_URI"] = 'mongodb+srv://user1:welcomeuser1@firstcluster-kacbb.mongodb.net/incident_manager?retryWrites=true&w=majority'
